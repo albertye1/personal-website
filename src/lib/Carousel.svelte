15eb30332ecee13e0cfc4933860948f662d7c694
Albert Ye <y.a.728@berkeley.edu>
2025-03-18 03:54:14 -0700

    Add static gallery, figure out animations later

---
+++ src/lib/Carousel.svelte
@@ -0,0 +1,73 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let currentIndex = 0;
+
+	export let images = Object.values(
+		import.meta.glob('$lib/assets/chaos/*.{png,jpg,jpeg,PNG,JPG,JPEG}', {
+			eager: true,
+			query: '?url',
+			import: 'default'
+		})
+	);
+
+	function next() {
+		currentIndex = (currentIndex + 1) % images.length;
+	}
+
+	function prev() {
+		currentIndex = (currentIndex - 1 + images.length) % images.length;
+	}
+</script>
+
+<div class="carousel">
+	<div class="carousel-images">
+		{#each [images[currentIndex]] as image, index (index)}
+			<img transition:fade src={image} alt="Carousel Image" />
+		{/each}
+	</div>
+	<button class="carousel-button prev" on:click={prev}>‹</button>
+	<button class="carousel-button next" on:click={next}>›</button>
+</div>
+
+<style>
+	.carousel {
+		position: relative;
+		width: 100%;
+		aspect-ratio: 16/9;
+		margin: auto;
+		overflow: hidden;
+		border-radius: 10px;
+		margin-bottom: 2em;
+	}
+
+	.carousel-images {
+		display: flex;
+		width: 100%;
+	}
+
+	.carousel-images img {
+		width: 100%;
+		aspect-ratio: 16/9;
+		object-fit: cover;
+	}
+
+	.carousel-button {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		background: rgba(0, 0, 0, 0.5);
+		color: white;
+		padding: 10px;
+		border: none;
+		cursor: pointer;
+	}
+
+	.prev {
+		left: 10px;
+	}
+
+	.next {
+		right: 10px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,20 @@
 	let { children } = $props();
 </script>
 
+<nav class="mx-[25%] mt-10 mb-5 py-10 sm:mx-[15%] lg:mx-[30%]">
+	<brand class="pr-5 text-xl">Albert Ye</brand>
+	<a class="px-1 !text-stone-700 hover:underline dark:!text-stone-300" href="/">home</a> /
+	<a class="px-1 !text-stone-700 hover:underline dark:!text-stone-300" href="/projects">projects</a>
+	/
+	<a class="px-1 !text-stone-700 hover:underline dark:!text-stone-300" href="/gallery">gallery</a> /
+	<a class="px-1 !text-stone-700 hover:underline dark:!text-stone-300" href="/blog">blog</a>
+</nav>
+
+<div class="px-[25%] sm:px-[15%] lg:px-[30%]">{@render children()}</div>
+
 <style>
-	a, a:hover{
+	a,
+	a:hover {
 		transition: 500ms;
 	}
 </style>
-
-
-<nav class="mx-60 mt-10 py-2 mb-5">
-	<brand class="text-xl pr-5">Albert Ye</brand>
-	<a class="px-1 hover:underline !text-stone-700 dark:!text-stone-300" href="/">home</a> /
-	<a class="px-1 hover:underline !text-stone-700 dark:!text-stone-300" href="/projects">projects</a> /
-    <a class="px-1 hover:underline !text-stone-700 dark:!text-stone-300" href="/gallery">gallery</a> /
-    <a class="px-1 hover:underline !text-stone-700 dark:!text-stone-300" href="/blog">blog</a>
-</nav>
-
-
-<div class="px-60">{@render children()}</div>
