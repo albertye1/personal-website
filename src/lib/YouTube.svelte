cb6ec82edda7b350dbc9b8648a5fc254249ad223
Albert Ye <me@albie.cat>
2026-06-08 19:35:25 -0700

    ski

---
+++ src/lib/YouTube.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let src;
+</script>
+
+<iframe
+	width="560"
+	height="315"
+	{src}
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
+></iframe> <br />
